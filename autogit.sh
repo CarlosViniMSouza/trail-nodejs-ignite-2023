16c7381954915f1c02045d020c76c1ee07a4fddc
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-15 20:48:42 -0300

    Prox. Aula: node --watch

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/trail-nodejs-ignite-2023.git
-git push -u origin main
+git commit -m "Prox. Aula: node --watch"
+git push
 git status
